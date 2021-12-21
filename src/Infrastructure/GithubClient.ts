630ec402fb514711a04f36db1f59d39866bd980f
Eber Herrera <eber.herrera@xing.com>
2021-12-21 12:28:06 -0300

    GithubCLient method implementation for send_challenge use-case

--- src/Infrastructure/GithubClient.ts
+++ src/Infrastructure/GithubClient.ts
@@ -10,21 +10,69 @@ const octokit = new Octokit({ auth: process.env.SLACK_CC_GITHUB_TOKEN });
 export class GithubClient implements GithubClientInterface {
 
     async createChallengeForCandidate(challenge: Challenge, candidate: Candidate): Promise<URL> {
-        throw new Error('not implemented');
+        try {
+            const { owner, repo } = this.parseOwnerAndRepo(challenge.getUrl()); 
+            const candidateGithubUsername = candidate.getGithubUser().getUsername();
+            const repoName = `${repo}_${candidateGithubUsername}`;
+
+            const response = await octokit.rest.repos.createUsingTemplate({
+                template_owner: owner, // eslint-disable-line camelcase
+                template_repo: repo, // eslint-disable-line camelcase
+                owner,
+                name: repoName,
+                private: true
+            });
+
+            if (!this.requestWasSuccess(response)) {
+                throw new Error(`GH Request error. Status code: ${response.status}`);
+            }
+
+            return new URL(response.data.html_url);
+        } catch (e) {
+            throw new GithubClientError(`Error while creating candidate repository. Error: ${e.message}`);
+        }
+    }
+
+    private requestWasSuccess(response):boolean {
+        return !!(response.status.toString()[0] == '2');
     }
 
     async githubRepositoryExists(url: URL): Promise<boolean> {
         try {
             const response = await octokit.rest.repos.get(this.parseOwnerAndRepo(url));
     
-            return response.status === 200;
+            return this.requestWasSuccess(response);
         } catch(e) {
             throw new GithubClientError(e.message);
         }
     }
 
     async addReviewersToCodeChallenge(candidateChallengeUrl: URL, reviewers: Reviewer[]): Promise<void> {
-        throw new Error('Method not implemented.');
+        try {
+            const { owner, repo } = this.parseOwnerAndRepo(candidateChallengeUrl);
+            const result = await Promise.allSettled(
+                reviewers.map((reviewer) => {
+                    return octokit.rest.repos.addCollaborator({
+                        owner,
+                        repo,
+                        username: reviewer.getGithubUser().getUsername()
+                    });
+                })
+            );
+
+            /** TODO: how do we control this error? */
+            /*
+            const allRequestOK = result.reduce(
+                (status, addOwnerReqResult) => status && this.requestWasSuccess(addOwnerReqResult), 
+                true);
+
+            if (!allRequestOK) {
+                console.warn(`Not all collaborators where added`)
+            }
+            */
+        } catch (e) {
+               throw new GithubClientError(`Error while adding collaborators to repo: ${e.message}`);
+        }
     }
 
     private parseOwnerAndRepo(url: URL): { owner: string, repo: string } {
--- tasks.md
+++ tasks.md
@@ -1,9 +1,10 @@
 # Pending tasks
-- Create ormconfig.js to depend on env vars OK
-- server container does not work to use typeorm commands OK
-- script in package.json to run typeorm OK
-- implement missing methods in GithubClient (explore use of templates)
+- ~~Create ormconfig.js to depend on env vars~~
+- ~~server container does not work to use typeorm commands~~
+- ~~script in package.json to run typeorm~~
+- ~~implement missing methods in GithubClient (explore use of templates)~~
 - Slack integration
+- Infrastructure folder tests (EoE ???)
 
 # Future features
 - MultiGH account installation?
